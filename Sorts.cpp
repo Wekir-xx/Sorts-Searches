2a39be909b4fee9a87708ce013b93fc303539b76
wekir <144891358+Wekir-xx@users.noreply.github.com>
2024-02-09 11:04:58 +0300

    Update Sorts.cpp

--- Sorts.cpp
+++ Sorts.cpp
@@ -5,7 +5,7 @@ void buble_sort(int* array, size_t size)
     for (size_t i = 0; i < size; i++)
     {
         bool tr = true;
-        for (size_t j = 0; j < size - 1; j++)
+        for (size_t j = 0; j < size - 1 - i; j++)
         {
             if (array[j] > array[j + 1])
             {
